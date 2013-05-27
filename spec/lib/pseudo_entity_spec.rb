98c02c16877b1a7557a5222886cabbeeaaf446d3
ChapterHouse <ChapterHouse.Dune@gmail.com>
2013-05-26 23:20:57 -0500

    Almost all instance methods defined directly by PseudoEntity tested.

--- spec/lib/pseudo_entity_spec.rb
+++ spec/lib/pseudo_entity_spec.rb
@@ -7,11 +7,35 @@ describe PseudoEntity do
     pseudo_entity.age.should == 10
   end
 
+  it "should combine components into a full address" do
+    pseudo_entity = PseudoEntity.new
+    pseudo_entity.stub(:street_address) { 'street_address' }
+    pseudo_entity.stub(:city) { 'city' }
+    pseudo_entity.stub(:state) { 'state' }
+    pseudo_entity.stub(:zip_code) { 'zip_code' }
+    pseudo_entity.address.should == "street_address\ncity, state. zip_code"
+  end
+
+  it "should identify when an address is and is not an apartment" do
+    pseudo_entity = PseudoEntity.new
+    pseudo_entity.stub(:street_modifier_needed) { :apartment }
+    pseudo_entity.apartment?.should == true
+    pseudo_entity.stub(:street_modifier_needed) { :something_else }
+    pseudo_entity.apartment?.should_not == true
+  end
+
   it "should extract birth year" do
     pseudo_entity = PseudoEntity.new
     date = 10.years.ago
     pseudo_entity.stub(:birth_day) { date }
     pseudo_entity.birth_year.should == date.year
+  end
+
+  it "should extract the last four digits of the credit card number" do
+    last_four = "4321"
+    pseudo_entity = PseudoEntity.new
+    pseudo_entity.stub(:credit_card_number) { "98765" + last_four }
+    pseudo_entity.credit_card_last_four.should == last_four
   end
 
   it "should combine names into a full name" do
@@ -51,6 +75,44 @@ describe PseudoEntity do
     pseudo_entity = PseudoEntity.new
     original = pseudo_entity
     pseudo_entity.reset!.should == original
+  end
+
+  it "should combine addresses in 2,1 order" do
+    pseudo_entity = PseudoEntity.new
+    pseudo_entity.stub(:street_address_line_1) { :street_address_line_1 }
+    pseudo_entity.stub(:street_address_line_2) { :street_address_line_2 }
+    pseudo_entity.street_address.should == 'street_address_line_2 street_address_line_1'
+  end
+
+  it "should trim missing street information" do
+    pseudo_entity = PseudoEntity.new
+    pseudo_entity.stub(:street_address_line_1) { :street_address_line_1 }
+    pseudo_entity.stub(:street_address_line_2) { '' }
+    pseudo_entity.street_address.should == 'street_address_line_1'
+    pseudo_entity.stub(:street_address_line_1) { '' }
+    pseudo_entity.stub(:street_address_line_2) { :street_address_line_2 }
+    pseudo_entity.street_address.should == 'street_address_line_2'
+  end
+
+  it "should identify when an address is and is not an suite" do
+    pseudo_entity = PseudoEntity.new
+    pseudo_entity.stub(:street_modifier_needed) { :suite }
+    pseudo_entity.suite?.should == true
+    pseudo_entity.stub(:street_modifier_needed) { :something_else }
+    pseudo_entity.suite?.should_not == true
+  end
+
+  it "should generate a yelp business id from the company name and the city" do
+    pseudo_entity = PseudoEntity.new
+    pseudo_entity.stub(:company_name) { "A B Co." }
+    pseudo_entity.stub(:city) { "St. Louis" }
+    pseudo_entity.yelp_business_id.should == "A-B-Co-St-Louis"
+  end
+
+  it "should generate a yelp business url from the yelp business id" do
+    pseudo_entity = PseudoEntity.new
+    pseudo_entity.stub(:yelp_business_id) { :yelp_business_id }
+    pseudo_entity.yelp_business_url.should == 'http://www.yelp.com/biz/yelp_business_id'
   end
 
   it "should generate a string 22 characters long for yelp user id" do
